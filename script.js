95835b452878e0c4a35c769e1e763e1732581054
riswan <riswanraunaqi@gmail.com>
2022-11-16 18:56:52 +0700

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("card1").addEventListener("click", function () {
+    let sd = document.getElementById("sd")
+    sd.classList.add("sd-img")
+})
+document.getElementById("card2").addEventListener("click", function () {
+    let sd = document.getElementById("smp")
+    sd.classList.add("smp-img")
+})
+document.getElementById("card3").addEventListener("click", function () {
+    let sd = document.getElementById("smk")
+    sd.classList.add("smk-img")
+})
